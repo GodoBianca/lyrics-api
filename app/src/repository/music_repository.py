6d614cd85ac8d59a0c41cd46c07e73595ae98a1a
GodoBianca <bianca.godo@outlook.com>
2024-09-30 22:57:49 -0300

    fix: correction repository save

--- app/src/repository/music_repository.py
+++ app/src/repository/music_repository.py
@@ -14,12 +14,8 @@ class MusicRepository:
         if music.id is None: #ve se o id existe se nao ele cria um novo
             music_id = uuid4()
             music.id = music_id
-            self.music_db[music_id] = music
-            return music
-        else: #se o id ja existir ele atualiza
-            self.music_db[music.id].title = music.title
-            self.music_db[music.id].content = music.content
-            return self.music_db[music.id]
+        self.music_db[music.id] = music
+        return self.music_db[music.id]
 
     def delete(self, id: UUID) -> None:
         if id in self.music_db:
